47870073c1be573584448ac2d8509f6d878dcfe4
Hirano Yuusuke <yuusuke.hirano5967@gmail.com>
2018-08-02 18:58:25 +0900

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Hirano on 2018/08/02.
+//  Copyright © 2018年 hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
